ae1990bdb8b88023b2ab06c0179c969bedd2cbb0
Hemanth Kumar <hemanth_kumar@anfcorp.com>
2018-08-08 19:55:00 -0400

    Added Swagger Documentation

--- src/main/java/nice/Application.java
+++ src/main/java/nice/Application.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import nice.models.Task;
+import nice.models.TaskRepository;
 import nice.models.User;
 import nice.models.UserRepository;
 
@@ -16,13 +18,18 @@ import nice.models.UserRepository;
 public class Application {
 
 	@Bean
-	CommandLineRunner init(UserRepository userDao) {
+	CommandLineRunner init(UserRepository userDao, TaskRepository taskRepo) {
 		return (evt) -> {
 			userDao.save(new User("niceUser1"));
 			userDao.save(new User("niceUser2"));
 			userDao.save(new User("niceUser3"));
+			
+			taskRepo.save(new Task("ProjectManagement","Project management","In Progress","niceUser1"));
+			taskRepo.save(new Task("Development","Development","In Progress","niceUser2"));
+			taskRepo.save(new Task("Testing","Testing","In Progress","niceUser3"));
 		};
 	}
+	
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
